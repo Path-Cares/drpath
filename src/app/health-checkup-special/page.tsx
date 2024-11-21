7038e95f0c4b90b780c439b76c9ba829d1524505
Developer <himanshu.masterbazar@gmail.com>
2024-11-21 11:58:28 +0530

    new

--- src/app/health-checkup-special/page.tsx
+++ src/app/health-checkup-special/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
-import BasicPackage from "@/components/pages/test/BasicPackage";
+// import BasicPackage from "@/components/pages/test/BasicPackage";
 import BasicPackageBanner from "@/components/pages/Services/BasicPackageBanner";
-import ThroidPackage from "@/components/pages/test/ThroidPackage";
-import ThroidDiabeticPackage from "@/components/pages/test/ThroidDiabeticPackage";
-import Healthcheckupspecialpackage from "@/components/pages/test/healthcheckupspecialpackage";
+// import Healthcheckupspecialpackage from "@/components/pages/test/healthcheckupspecialpackage";
+// import ThroidPackage from "@/components/pages/test/ThroidPackage";
+// import ThroidDiabeticPackage from "@/components/pages/test/ThroidDiabeticPackage";
+// import Healthcheckupspecialpackage from "@/components/pages/test/healthcheckupspecialpackage";
+import Healthcheckupspecialpackage from "@/components/pages/test/Healthcheckupspecialpackage";
+
 
 
 function page() {
